c393c71b433561e02546f7958a93ece0c5661654
Jason  Montes <jasone.montes@gmail.com>
2019-05-25 15:40:19 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,6 +1,17 @@
 class Patient
   
-  
+  @@all = []
+
+  attr_accessor :doctor, :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end 
   
   
 end
